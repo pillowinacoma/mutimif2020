87010084fb0b19d1dbae6b41741749cd35d1c6d2
PHAM Vincent <vincent.pham@etu.univ-lyon1.fr>
2020-11-23 00:11:16 +0100

    Ajout des test DAO

---
+++ server/src/test/java/fr/univlyon1/m1if/m1if10/EventDaoTest.java
@@ -0,0 +1,60 @@
+package fr.univlyon1.m1if.m1if10;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import fr.univlyon1.m1if.m1if10.dao.EventDAO;
+import fr.univlyon1.m1if.m1if10.model.Event;
+
+class EventDaoTest {
+	
+	public EventDAO eDAO;
+	public EntityManager em;
+	
+	@BeforeEach
+	public void setUp() {
+		em = Persistence.createEntityManagerFactory("public").createEntityManager();
+		em.getTransaction().begin();
+		eDAO = new EventDAO(em);
+	}
+
+	@Test
+	void getOrCreateTest() {
+		//Given
+		Event e;
+		
+		//When
+		e = eDAO.getOrCreate("Nuit de l'info");
+		em.getTransaction().commit();
+		
+		//Then
+		assertEquals("Nuit de l'info" ,e.getName());
+		
+		em.close();
+	}
+	
+	@Test
+	void getEventByNameTest() {
+		//Given
+		Event e1, e2, e3;
+		
+		//When
+		e1 = eDAO.getOrCreate("Presentation du M2");
+		e2 = eDAO.getOrCreate("Gala");
+		e3 = eDAO.getOrCreate("Remise des diplomes");
+		em.getTransaction().commit();
+		
+		//Then
+		assertEquals(null,eDAO.getEventByName("Galalala"));
+		assertEquals(e1,eDAO.getEventByName("Presentation du M2"));
+		assertEquals(e3,eDAO.getEventByName(e3.getName()));
+		
+		em.close();
+	}
+}
---
+++ server/src/test/java/fr/univlyon1/m1if/m1if10/HashtagDaoTest.java
@@ -0,0 +1,63 @@
+package fr.univlyon1.m1if.m1if10;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import fr.univlyon1.m1if.m1if10.dao.HashtagDAO;
+import fr.univlyon1.m1if.m1if10.model.Hashtag;
+
+
+class HashtagDaoTest {
+	
+	public HashtagDAO htDAO;
+	public EntityManager em;
+	
+	@BeforeEach
+	void setUp() {
+		em = Persistence.createEntityManagerFactory("public").createEntityManager();
+		em.getTransaction().begin();
+		htDAO = new HashtagDAO(em);
+	}
+
+	@Test
+	void getOrCreateTest() {
+		//Given
+		Hashtag h;
+		
+		//When
+		h = htDAO.getOrCreate("Covid19");
+		em.getTransaction().commit();
+		
+		//Then
+		assertEquals("Covid19",h.getName());
+		assertEquals(h, htDAO.getOrCreate("Covid19"));
+		
+		em.close();
+	}
+	
+	@Test
+	void getHashtagByNameTest() {
+		//Given
+		Hashtag h1,h2,h3;
+		
+		//When
+		h1 = htDAO.getOrCreate("Master1");
+		h2 = htDAO.getOrCreate("oof");
+		h3 = htDAO.getOrCreate("Redoublement");
+		em.getTransaction().commit();
+		
+		//Then
+		assertEquals(h1,htDAO.getHashtagByName("Master1"));
+		assertEquals(null,htDAO.getHashtagByName("foo"));
+		assertEquals(h3,htDAO.getHashtagByName(h3.getName()));
+		
+		em.close();
+	}
+
+}
